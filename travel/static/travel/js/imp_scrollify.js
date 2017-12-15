5741c0f6581acc81239d42a4fe6cb8ffb2c5a8ce
jakesmorrison <jakesmorrison4@gmail.com>
2017-12-15 08:32:30 -0700

    fixing scrollify on big browser

--- travel/static/travel/js/imp_scrollify.js
+++ travel/static/travel/js/imp_scrollify.js
@@ -10,27 +10,28 @@ $(window).resize(function() {
   windowsize = $(window).width();
   if (windowsize <= 1200) {
       $('.my_data_col').addClass('my_row');
+      $(function() {
+        $.scrollify({
+            section : ".my_row",
+            interstitialSection : "",
+            easing: "easeOutExpo",
+            scrollSpeed: 1100,
+            offset : 0,
+            scrollbars: true,
+            standardScrollElements: "",
+            setHeights: true,
+            overflowScroll: true,
+            updateHash: true,
+            touchScroll:true,
+            before:function() {},
+            after:function() {},
+            afterResize:function() {},
+            afterRender:function() {}
+
+        });
+    });
+
   }
 });
 
 
-$(function() {
-    $.scrollify({
-        section : ".my_row",
-        interstitialSection : "",
-		easing: "easeOutExpo",
-		scrollSpeed: 1100,
-		offset : 0,
-		scrollbars: true,
-		standardScrollElements: "",
-		setHeights: true,
-		overflowScroll: true,
-		updateHash: true,
-		touchScroll:true,
-		before:function() {},
-		after:function() {},
-		afterResize:function() {},
-		afterRender:function() {}
-
-    });
-});
