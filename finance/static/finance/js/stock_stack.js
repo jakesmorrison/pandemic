7c7cb69e7ede30d27d62faee1c9d2ed85f735b41
jakesmorrison <jakesmorrison4@gmail.com>
2018-02-03 16:33:54 -0700

    stock_tracker

--- finance/static/finance/js/stock_stack.js
+++ finance/static/finance/js/stock_stack.js
@@ -6,7 +6,7 @@ function plot_stock_stack(stack_data, dates){
         exporting: { enabled: false },
         credits: {enabled: false },
         title: {
-            text: 'Stacked column chart'
+            text: 'Daily Change by Stock Transaction'
         },
         xAxis: {
             categories: dates
@@ -25,31 +25,21 @@ function plot_stock_stack(stack_data, dates){
         },
         legend: {
             enabled: false
-//            align: 'right',
-//            x: -30,
-//            verticalAlign: 'top',
-//            y: 25,
-//            floating: true,
-//            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-//            borderColor: '#CCC',
-//            borderWidth: 1,
-//            shadow: false
         },
         tooltip: {
-//            headerFormat: '<b>{point.x}</b><br/>',
-//            pointFormat: '{series.name} : {point.y} <br> Initial Cost: {series.cost} <br> Shares: {series.quanity} <br> Buy Date: {series.buydate} <br> Sell Date: {series.selldate}'
-            //Total: {point.stackTotal}
             useHTML: true,
             formatter: function () {
-                current_price = Math.round((this.point.cost + this.point.y)*100)/100
-                table = "<table class='tt_table'><tr><td>Daily Change</td><td>"+this.point.y+"</td></tr><tr><td>Current Price</td><td>$"+current_price+"</td></tr><tr><td>Initial Price</td><td>$"+this.point.cost+"</td></tr><tr><td>Quanity</td><td>"+this.point.quanity+"</td></tr><tr><td>Buy Date</td><td>"+this.point.buydate+"</td></tr><tr><td>Sell Date</td><td>"+this.point.selldate+"</td></tr></table>"
-                return '<i>'+this.point.category+'</i><br><span class="tt_header">' + this.series.name + '</span><br>' +table
+                if ( this.point.y != 0 ){
+                    current_price = Math.round((this.point.cost + this.point.y)*100)/100
+                    table = "<table class='tt_table'><tr><td>Daily Change</td><td>"+this.point.y+"</td></tr><tr><td>Current Price</td><td>$"+current_price+"</td></tr><tr><td>Initial Price</td><td>$"+this.point.cost+"</td></tr><tr><td>Quanity</td><td>"+this.point.quanity+"</td></tr><tr><td>Buy Date</td><td>"+this.point.buydate+"</td></tr><tr><td>Sell Date</td><td>"+this.point.selldate+"</td></tr><tr><td>Transaction ID</td><td>"+this.point.transaction+"</td></tr></table>"
+                    return '<i>'+this.point.category+'</i><br><span class="tt_header">' + this.series.name + '</span><br>' +table
+                }
             }
         },
         plotOptions: {
             column: {
                 stacking: 'normal',
-                borderColor: '#FFFFFF',
+                borderColor: '#ffffff',
                 borderWidth: 5
             }
         },
