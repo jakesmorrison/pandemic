cb7e92ac99fce5596d780b6bfbbb5fa5fb948982
jakesmorrison <jakesmorrison4@gmail.com>
2018-02-03 17:24:33 -0700

    stock_tracker

--- finance/static/finance/js/stock_stack.js
+++ finance/static/finance/js/stock_stack.js
@@ -1,5 +1,5 @@
 function plot_stock_stack(stack_data, dates){
-    Highcharts.chart('stock_stacked', {
+    var chart = Highcharts.chart('stock_stacked', {
         chart: {
             type: 'column'
         },
@@ -31,15 +31,20 @@ function plot_stock_stack(stack_data, dates){
             formatter: function () {
                 if ( this.point.y != 0 ){
                     current_price = Math.round((this.point.cost + this.point.y)*100)/100
-                    table = "<table class='tt_table'><tr><td>Daily Change</td><td>"+this.point.y+"</td></tr><tr><td>Current Price</td><td>$"+current_price+"</td></tr><tr><td>Initial Price</td><td>$"+this.point.cost+"</td></tr><tr><td>Quanity</td><td>"+this.point.quanity+"</td></tr><tr><td>Buy Date</td><td>"+this.point.buydate+"</td></tr><tr><td>Sell Date</td><td>"+this.point.selldate+"</td></tr><tr><td>Transaction ID</td><td>"+this.point.transaction+"</td></tr></table>"
-                    return '<i>'+this.point.category+'</i><br><span class="tt_header">' + this.series.name + '</span><br>' +table
+                    table = "<table class='tt_table'><tr><td class='table_left'>Daily Change</td><td>"+this.point.y+"</td></tr><tr><td class='table_left'>Current Price</td><td>$"+current_price+"</td></tr><tr><td class='table_left'>Initial Price</td><td>$"+this.point.cost+"</td></tr><tr><td class='table_left'>Quanity</td><td>"+this.point.quanity+"</td></tr><tr><td class='table_left'>Buy Date</td><td>"+this.point.buydate+"</td></tr><tr><td class='table_left'>Sell Date</td><td>"+this.point.selldate+"</td></tr><tr><td class='table_left'>Transaction ID</td><td>"+this.point.transaction+"</td></tr></table>"
+                    header = '<p class="date"><i>'+this.point.category+'</i></p><br><h4 class="tt_header">' + this.series.name + '</h4>'
+                    $( ".my_table" ).html(header);
+                    $( ".my_table" ).append(table);
+                    return '<i>'+this.point.category+'</i><br><span class="tt_header">' + this.series.name + '</span>'
+                }
+                else{
                 }
             }
         },
         plotOptions: {
             column: {
                 stacking: 'normal',
-                borderColor: '#ffffff',
+                borderColor: '#fff',
                 borderWidth: 5
             }
         },
