268d5cbf4eac57b3aa917b4525d5fb183aabd434
jakesmorrison <jakesmorrison4@gmail.com>
2018-03-03 14:37:56 -0700

    adding oscar

--- oscar/models.py
+++ oscar/models.py
@@ -9,16 +9,17 @@ class OscarCategories(models.Model):
     def __str__(self):
         return "{} {} {}".format(self.Year,self.Cat,self.Name)
 
-# class Winners(models.Model):
-#     Year = models.IntegerField()
-#     Cat = models.CharField(max_length=1000)
-#     Name = models.CharField(max_length=1000, default="")
-#     Weight = models.IntegerField()
-#     def __str__(self):
-#         return "{} {} {} {}".format(self.Year,self.Cat,self.Name,self.Weight)
-#
-# class Users(models.Model):
-#     User = models.CharField(max_length=1000)
-#     Selections = PickledObjectField(default=[])
-#     def __str__(self):
-#         return "{} {}".format(self.User,self.Selections)
+class Winners(models.Model):
+    Year = models.IntegerField()
+    Cat = models.CharField(max_length=1000)
+    Name = models.CharField(max_length=1000, default="")
+    Weight = models.IntegerField()
+    def __str__(self):
+        return "{} {} {} {}".format(self.Year,self.Cat,self.Name,self.Weight)
+
+class Users(models.Model):
+    User = models.CharField(max_length=1000)
+    Selections = PickledObjectField(default=[])
+    def __str__(self):
+        return "{} {}".format(self.User,self.Selections)
+
