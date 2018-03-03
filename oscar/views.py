2dc12151583a044dd23b627703bcc7ddf6c716dc
jakesmorrison <jakesmorrison4@gmail.com>
2018-03-03 12:19:12 -0700

    adding oscar

--- oscar/views.py
+++ oscar/views.py
@@ -6,14 +6,12 @@ from .models import OscarCategories
 
 def oscar(request):
     df = pd.DataFrame.from_records(OscarCategories.objects.all().values())
-    df = df[df["Year"]==2017]
+    df = df[df["Year"]==2018]
     cat = df["Cat"].tolist()
 
     my_dict = {}
     for x in cat:
         my_dict[x] = df[df["Cat"]==x]["Name"].tolist()
-
-    print(my_dict)
 
     context = {
         'oscar_options': my_dict
