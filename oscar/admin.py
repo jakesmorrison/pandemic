4036fff03ad00a75b769e3fa9b6a760d2f743155
jakesmorrison <jakesmorrison4@gmail.com>
2018-03-04 09:33:53 -0700

    adding oscar

--- oscar/admin.py
+++ oscar/admin.py
@@ -6,18 +6,21 @@ admin.site.register(
     OscarCategories,
     list_display="Year,Cat,Name".split(","),
     list_display_links="Year,Cat,Name".split(","),
+    search_fields ="Year,Cat,Name".split(","),
 )
 
 admin.site.register(
     Winners,
     list_display="Year,Cat,Name,Weight".split(","),
     list_display_links="Year,Cat,Name,Weight".split(","),
+    search_fields ="Year,Cat,Name,Weight".split(","),
 )
 
 admin.site.register(
     Users,
     list_display="Year,User,Cat,Won,Favorite".split(","),
     list_display_links="Year,User,Cat,Won,Favorite".split(","),
+    search_fields = "Year,User,Cat,Won,Favorite".split(","),
 )
 
 
