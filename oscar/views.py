67e3a2bf6f0172141f69d2288b1542205baa0d44
jakesmorrison <jakesmorrison4@gmail.com>
2018-03-04 10:55:12 -0700

    adding oscar

--- oscar/views.py
+++ oscar/views.py
@@ -18,29 +18,29 @@ def oscar(request):
     df_winners = pd.DataFrame.from_records(Winners.objects.all().values())
     df_users = pd.DataFrame.from_records(Users.objects.all().values())
 
-    df_merge = df_users.merge(df_winners, left_on='Cat', right_on='Cat', how='outer')
-    del df_merge["Year_x"]
-    del df_merge["Year_y"]
-    del df_merge["id_x"]
-    del df_merge["id_y"]
-    del df_merge["Favorite"]
-    df_merge = df_merge.reset_index()
-
-    df_merge["Points"] = np.where((df_merge['Won'] == df_merge['Name']), 1*df_merge["Weight"], 0)
-
-    rankings = df_merge.groupby(["User"]).sum().reset_index()
-    del rankings["index"]
-    del rankings["Weight"]
-
-    rankings = rankings.sort_values(by=['Points'], ascending=False).reset_index()
-    rankings["index"] = rankings["index"] + 1
-    rankings.columns = ['Rank', 'User', 'Points']
-    rankings = rankings.to_html(index=False, classes="rank_table")
-    rankings = rankings.replace("<tr>",'<tr class="table_content table-bordered">')
-    rankings = rankings.replace('border="1"', "")
-    rankings = rankings.replace('<tr style="text-align: right;">','<tr class="table_content">')
-
-    our_favs = df_users.groupby(["Cat","Favorite"]).count().reset_index()
+    # df_merge = df_users.merge(df_winners, left_on='Cat', right_on='Cat', how='outer')
+    # del df_merge["Year_x"]
+    # del df_merge["Year_y"]
+    # del df_merge["id_x"]
+    # del df_merge["id_y"]
+    # del df_merge["Favorite"]
+    # df_merge = df_merge.reset_index()
+    #
+    # df_merge["Points"] = np.where((df_merge['Won'] == df_merge['Name']), 1*df_merge["Weight"], 0)
+    #
+    # rankings = df_merge.groupby(["User"]).sum().reset_index()
+    # del rankings["index"]
+    # del rankings["Weight"]
+    #
+    # rankings = rankings.sort_values(by=['Points'], ascending=False).reset_index()
+    # rankings["index"] = rankings["index"] + 1
+    # rankings.columns = ['Rank', 'User', 'Points']
+    # rankings = rankings.to_html(index=False, classes="rank_table")
+    # rankings = rankings.replace("<tr>",'<tr class="table_content table-bordered">')
+    # rankings = rankings.replace('border="1"', "")
+    # rankings = rankings.replace('<tr style="text-align: right;">','<tr class="table_content">')
+    #
+    # our_favs = df_users.groupby(["Cat","Favorite"]).count().reset_index()
 
 
     my_dict = {}
