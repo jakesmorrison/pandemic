fd0c836506e982e2092e2f6735815be155c6dce4
jakesmorrison <jakesmorrison4@gmail.com>
2018-04-05 10:25:36 -0600

    adding csv downloader to travel

--- travel/admin.py
+++ travel/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .actions import export_as_csv_action
 
 # Register your models here.
 
@@ -24,6 +25,7 @@ admin.site.register(Issued,IssuedModelAdmin)
 
 class TravelStatsModelAdmin(admin.ModelAdmin):
     list_display = "self.date, self.city, self.country, self.traveling_with, self.wake_time, self.sleep_time, self.naps, self.breakfast_time, self.breakfast_cost, self.breakfast_type, self.lunch_time, self.lunch_cost, self.lunch_type, self.dinner_time, self.dinner_cost, self.dinner_type, self.glasses_of_wine, self.wine_cost, self.cocktail_count, self.cocktail_cost, self.miles_air, self.miles_train, self.miles_bus, self.miles_car, self.miles_walk, self.housing_cost, self.housing_name, self.housing_type, self.num_sex, self.num_master, self.num_poops, self.hours_read, self.book, self.show, self.hours_tv, self.num_pictures_taken, self.hours_podcast, self.podcast, self.hours_music, self.museums_visited, self.friends_met, self.tour_name, self.tour_cost".replace(" ", "").replace("self.","").split(",")
+    actions = [export_as_csv_action("CSV Export", fields=list_display)]
     search_fields = list_display
 from .models import TravelStats
 admin.site.register(TravelStats,TravelStatsModelAdmin)
