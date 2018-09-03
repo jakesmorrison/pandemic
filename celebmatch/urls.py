402bade86ccffe5aefd2b597c86c29e12d8ecf77
jakesmorrison <jakesmorrison4@gmail.com>
2018-09-03 01:05:11 -0600

    adding celeb lookalike

--- celebmatch/urls.py
+++ celebmatch/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'celebmatch'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^retrieve_image', views.retrieve_image, name='retrieve_image'),
+    url(r'^retrieve_image.*', views.retrieve_image, name='retrieve_image'),
 ]
