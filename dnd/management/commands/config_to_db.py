e35ff8ec3b1ff0ba9dad83ca0324e3f7260f085c
jakesmorrison <jakesmorrison4@gmail.com>
2019-01-04 09:49:46 -0700

    pandemic

--- dnd/management/commands/config_to_db.py
+++ dnd/management/commands/config_to_db.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        self.config_to_db()
+        self.add_spells()
 
     def config_to_db(self):
         for key, val in config.my_character_dict.items():
@@ -51,3 +51,23 @@ class Command(BaseCommand):
                 level_9="",
                 other="",
             )
+
+    def add_spells(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_name = dir_path + "/../../static/dnd/csv/spells.pickle"
+        with open(file_name, 'rb') as handle:
+            b = pickle.load(handle)
+
+        for key, val in b.items():
+            (spell_instance, new_spell) = Spells.objects.update_or_create(
+                spell=key.replace("&rsquo;","'").replace("&#8217;","'").replace("&#039;", "'").replace("&nbsp;", " ").replace("&mdash;", "-").replace("&ndash;", "-"),
+                spell_type=val["spell_type"].replace("&rsquo;","'").replace("&#8217;","'").replace("&#039;", "'").replace("&nbsp;", " ").replace("&mdash;", "-").replace("&ndash;", "-"),
+                level=val["level"].replace("&rsquo;","'").replace("&#8217;","'").replace("&#039;", "'").replace("&nbsp;", " ").replace("&mdash;", "-").replace("&ndash;", " "),
+                casting_time=val["casting_time"].replace("&rsquo;","'").replace("&#8217;","'").replace("&#039;", "'").replace("&nbsp;", " ").replace("&mdash;", "-").replace("&ndash;", "-"),
+                range=val["range"].replace("&rsquo;","'").replace("&#8217;","'").replace("&#039;", "'").replace("&nbsp;", " ").replace("&mdash;", "-").replace("&ndash;", "-"),
+                components=val["components"].replace("&rsquo;","'").replace("&#8217;","'").replace("&#039;", "'").replace("&nbsp;", " ").replace("&mdash;", "-").replace("&ndash;", "-"),
+                duration=val["duration"].replace("&rsquo;","'").replace("&#8217;","'").replace("&#039;", "'").replace("&nbsp;", " ").replace("&mdash;", "-").replace("&ndash;", "-"),
+                description=val["description"].replace("&rsquo;","'").replace("&#8217;","'").replace("&#039;", "'").replace("&nbsp;", " ").replace("&mdash;", "-").replace("&ndash;", "-"),
+                whose_spell=", ".join(val["whose_spell"]),
+                page=val["page"].replace("&rsquo;","'").replace("&#8217;","'").replace("&#039;", "'").replace("&nbsp;", " ").replace("&mdash;", "-").replace("&ndash;", "-"),
+
