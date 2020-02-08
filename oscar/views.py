471d7b5a7d9604d77c0b139bb4c16a880c3074bc
JAKOB MORRISON <JMORRISON@MICRON.COM>
2020-02-08 14:53:57 -0700

    2020 oscar update

--- oscar/views.py
+++ oscar/views.py
@@ -8,7 +8,7 @@ from collections import OrderedDict
 
 # Create your views here
 
-year = 2019
+year = 2020
 
 def oscar(request):
     df = pd.DataFrame.from_records(OscarCategories.objects.all().values())
