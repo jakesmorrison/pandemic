3fb56bc43bad47c61c6a273d744e2da34c92ac63
Jake Morrison <jakesmorrison4@gmail.com>
2021-04-25 10:50:10 -0600

    Update oscar_csv_to_db.py

--- oscar/management/commands/oscar_csv_to_db.py
+++ oscar/management/commands/oscar_csv_to_db.py
@@ -19,7 +19,7 @@ class Command(BaseCommand):
 
     def add_oscar(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        file_name = dir_path + "/../../static/oscar/csv/Oscars_2019.csv"
+        file_name = dir_path + "/../../static/oscar/csv/Oscars_2021.csv"
 
         df = pd.read_csv(file_name)
         df.columns = ["Year", "Cat", "Selection"]
@@ -41,7 +41,7 @@ class Command(BaseCommand):
 
         catarr = {"Best Picture": 3, "Lead Actor": 3, "Lead Actress": 3, "Supporting Actor": 3, "Supporting Actress": 3,
                   "Director": 3, "Animated Feature": 3, "Animated Short": 1 , "Adapted Screenplay": 3, "Original Screenplay": 3,
-                  "Cinematography": 2, "Best Documentary Feature": 1, "Best Live Action Short Film": 1, "Best Foreign Language Film": 1,
+                  "Cinematography": 2, "Best Documentary Feature": 2, "Best Live Action Short Film": 1, "Best Foreign Language Film": 2,
                   "Film Editing": 2, "Sound Editing": 2, "Sound Mixing": 2, "Production Design": 2,
                   "Original Score": 2, "Original Song": 2, "Makeup and Hair": 2, "Costume Design": 2, "Visual Effects": 2,
                   "Best Documentary Short Subject": 1}
